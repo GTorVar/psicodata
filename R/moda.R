161a461c6f9a9a1d9fa283b009e756bd5a000710
GTorVar <torresgvaras@gmail.com>
2024-08-28 21:50:16 +0200

    Initial commit

---
+++ R/moda.R
@@ -0,0 +1,45 @@
+#' Mode of a vector or data frame
+#'
+#' If the input is a data frame, it calcule the mode of each column. If there are many modes, only returns the first.
+#'
+#' @param x The vetor or data frame
+#'
+#' @return The mode as a vector or in a data frame. If there are multiples modes, it return a list with the variables with multiples modes.
+#' @export
+#'
+#' @examples
+#' x <- data.frame(A = c(1,1,2,3), B=c(2,2,3,4))
+#' moda(x)
+moda <- function(x){
+   #Sirve para vectores y data.frames
+   if (is.vector(x)) {
+      completa <- as.data.frame(table(x))
+      sin_primera_moda <- as.data.frame(table(x[-(as.numeric(names(which.max(table(x)))))]))
+      if (completa$Freq[which.max(completa$Freq)]==sin_primera_moda$Freq[which.max(sin_primera_moda$Freq)]) {
+         retorno <- list(result = as.numeric(names(which.max(table(x)))), warning = 'Varias modas')
+         return(retorno)
+      } else {
+         return(as.numeric(names(which.max(table(x)))))
+      }
+   } else  {
+      resultado <- matrix(ncol = ncol(x))
+      mensaje <- c()
+      for (i in seq_along(colnames(x))) {
+         completa <- as.data.frame(table(x[i]))
+         sin_primera_moda <- as.data.frame(completa[-which.max(completa$Freq),])
+         if (completa$Freq[which.max(completa$Freq)]==sin_primera_moda$Freq[which.max(sin_primera_moda$Freq)]) {
+            resultado[i] <- as.numeric(names(which.max(table(x[i]))))
+            mensaje[i] <- paste(colnames(x[i]), ' tiene varias modas')
+         } else {
+            resultado[i] <- as.numeric(names(which.max(table(x[i]))))
+            mensaje[i] <- ''
+         }
+      }
+      mensaje <- mensaje[mensaje!='']
+      resultado <- as.data.frame(resultado)
+      colnames(resultado) <- colnames(x)
+      rownames(resultado) <- 'Moda'
+      retorno <- list(result = resultado, warning = mensaje)
+      return(retorno)
+   }
+}
